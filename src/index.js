fc7e82d955ed69a5a89c4184d057c75cac84e2a4
Ivan Chuvilko <61088547+imch97@users.noreply.github.com>
2020-10-27 17:11:25 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+ class Schet extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {kol: 0};
+    
+       
+    this.plusClick = this.plusClick.bind(this);
+    this.minusClick = this.minusClick.bind(this);
+ }
+
+ plusClick(){
+  this.setState(state => ({      kol: state.kol + 1    }));
+ }
+
+ minusClick(){
+  this.setState(state => ({      kol: state.kol - 1    }));
+ }
+
+ render(){
+   return(
+     <div>
+   <h1>{this.state.kol}</h1>
+   <button onClick={this.plusClick}>+</button>
+   <button onClick={this.minusClick}>-</button>
+   </div>
+   );
+ }
+
+}
+
+ReactDOM.render(
+  <Schet />,
+  document.getElementById('root')
+);
+
+
